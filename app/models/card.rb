1b007f8c3c96319d5347af51cf79bfdecc5dd1f7
Alex Satterly <alcsatt@gmail.com>
2015-06-12 14:38:35 -0400

    fix typo in card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ActiveRecord::Base
   belongs_to :default_deck, foreign_key: "deck_id"
-  belongs_to :personal_decks, foreign_key: "deck_id"
+  belongs_to :personal_deck, foreign_key: "deck_id"
 end
